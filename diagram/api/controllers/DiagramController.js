5303e3b2f4f9641fae7b81625862503b577150f1
mansrz <man.srz@gmail.com>
2016-01-24 18:42:37 -0500

    Changes in Controllers and model of Diagram

--- diagram/api/controllers/DiagramController.js
+++ diagram/api/controllers/DiagramController.js
@@ -6,6 +6,37 @@
  */
 
 module.exports = {
-	
+  getDiagramasByUser: function(req, res){
+    console.log(req.session.user);
+    console.log(req.session);
+    Diagram.find({autor:req.session.user}).exec(function(err, found){
+      return res.json({result: found})
+    });
+  },
+  createDiagram : function(req, res){
+    var name = req.param('name');
+    var autor = req.param('autor');
+    var content = req.param('content');
+    var id = req.param('id');
+    Diagram.findOne({id:id}).exec(function(err, created){
+      console.log(err);
+      created.content = content;
+      created.name = name;
+      created.save(function(err,s){
+        return res.json({new:created});
+      });
+    });
+  },
+  showDiagram: function(req, res){
+    var id = req.param('id');
+    if(id == null){
+      Diagram.create({autor:req.session.user,name:''}).exec(function(err, result){
+        return res.view('workspace_material.ejs',{id:result.id})
+      });
+    }else{
+      console.log(id);
+      return res.view('workspace_material.ejs',{id:id})
+    }
+  }
 };
 
--- diagram/api/models/Diagram.js
+++ diagram/api/models/Diagram.js
@@ -3,7 +3,10 @@ module.exports ={
     attributes:{
         name : 'string',
         autor: 'string',
-        content: 'json',
+        content: {
+          type:'string',
+          maxLength: 23365000,
+        },
     },
 
 };
