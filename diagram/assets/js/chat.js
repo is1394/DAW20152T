15104f9b092247b5c0c5b8de1fd95fdeeab732e1
is1394 <israel1394@gmail.com>
2016-01-25 10:25:05 -0500

    web socket for chat with errors :(

--- diagram/assets/js/chat.js
+++ diagram/assets/js/chat.js
@@ -1,11 +1,10 @@
-$("#submitmsg").click(function(){
-		var clientmsg = $("#usermsg").val();
-        console.log(clientmsg);
-        newsentmessage($("#usermsg").val());
-		$("#usermsg").val('');
+var chatlist = [];
+var allChat= function(){io.socket.get('/chat/addconv');
 
-	});
-
+    $.get('/chat',function(data){
+        chatlist = data;
+        console.log(data);
+});};
 
 function newsentmessage(texto){
     sentcont = $("<div></div>").attr("class","sent message-content col s12 m12 l12");
@@ -25,3 +24,37 @@ function newsentmessage(texto){
 
     $("#chatbox").append(sentcont);
 }
+
+allChat();
+
+// function makechanges(data){
+//
+// }
+
+io.socket.on('/chat',function(obj){
+    if(obj.verb === 'created'){
+        console.log(obj);
+        chatlist.push(obj.data);
+
+        // makechanges(obj.data);
+        newsentmessage(obj.data.message);
+    }
+});
+
+
+
+
+
+$("#submitmsg").click(function(){
+		var clientmsg = $("#usermsg").val();
+        console.log(clientmsg);
+        newsentmessage($("#usermsg").val());
+        // $.post("/chat/addconv",{user: $("#user_name").text(), message: clientmsg, diagram_id: $('#id_diagram').val()},function(data){console.log(data);});
+        io.socket.post("/chat/addconv",{user: $("#user_name").text(), message: clientmsg, diagram_id: $('#id_diagram').val()},function(resData,jwres){
+            console.log(resData);
+            $("#usermsg").val('');
+        });
+
+
+
+	});
